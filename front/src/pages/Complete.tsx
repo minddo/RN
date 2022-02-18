f5e30e8cd7e63fde186a82dfd4cad66ea4c1c258
Minhyung <mindrome95@kaist.ac.kr>
2022-02-18 14:00:09 +0900

    remvoe .git in front

---
+++ front/src/pages/Complete.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Complete() {
+  return (
+    <View>
+      <Text>완료화면2</Text>
+    </View>
+  );
+}
+
+export default Complete;
